d2b81e424a87541b2fedb2d640d5bc09d8afdaa3
marie bertrand <marie.bertrand@epfedu.fr>
2023-09-18 12:56:32 +0200

    Création d'un nouveau projet versionné exo1 pas terminé

---
+++ TP0_BERTRAND/exo1/src/exo1/Exo1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+/*bertrand
+tdB
+afficher un message
+18/09/2023
+*/
+/**
+ *
+ * @author marie
+ */
+public class Exo1 {
+
+    
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+    }
+    
+}
