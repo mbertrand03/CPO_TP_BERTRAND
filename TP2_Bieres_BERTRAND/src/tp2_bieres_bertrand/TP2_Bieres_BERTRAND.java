c32e83273ba666cbc2738cf9fedb4af3664e6160
marie bertrand <marie.bertrand@epfedu.fr>
2023-10-06 17:49:59 +0200

    tp2 début

---
+++ TP2_Bieres_BERTRAND/src/tp2_bieres_bertrand/TP2_Bieres_BERTRAND.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_bertrand;
+
+/**
+ *bertrand
+ * 06/10/2023
+ * tdB
+ * création de class et d'objets
+ * @author marie
+ */
+public class TP2_Bieres_BERTRAND {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
