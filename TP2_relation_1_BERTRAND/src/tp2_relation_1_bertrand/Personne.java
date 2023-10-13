a2b2d335026085b26683be8878ba11df8060fe6b
marie bertrand <marie.bertrand@epfedu.fr>
2023-10-13 11:15:53 +0200

    tp2 ex1 fait TP2 exo2 fait tp2 exo3 fait

---
+++ TP2_relation_1_BERTRAND/src/tp2_relation_1_bertrand/Personne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_bertrand;
+
+/**
+ *
+ * @author marie
+ */
+public class Personne {
+    String nom;
+    String prenom;
+    
+    public String toString () {
+    return "la personne s'appelle: "+ nom +prenom;
+    }
+    
+    
+}
---
+++ TP2_relation_1_BERTRAND/src/tp2_relation_1_bertrand/Voiture.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_bertrand;
+
+/**
+ *
+ * @author marie
+ */
+public class Voiture {
+    String Modele;
+    String Marque;
+    int PuissanceCV;
+    
+    public String toString () {
+    return "le modele de la voiture est:"+Modele+"sa marque est:"+Marque+"la puissanceCV est: "+ PuissanceCV;
+    }
+    
+    
+}
